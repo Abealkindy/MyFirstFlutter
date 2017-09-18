75aa86b5c5931bcf6ab53fc577187ce47e0a584c
Abealkindy <abealkindy@gmail.com>
2017-09-18 21:21:37 +0700

    Initial commit

---
+++ android/app/src/main/kotlin/abraham24/com/myfluttersandbox/MainActivity.kt
@@ -0,0 +1,13 @@
+package abraham24.com.myfluttersandbox
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity(): FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
